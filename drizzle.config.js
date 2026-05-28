2e597a518518ff5816f0a870dd68e1d983173342
nikit-dwivedi <nikitdwivedi0612@gmail.com>
2026-05-28 21:57:24 +0530

    change

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default defineConfig({
     schema: './src/db/schema.ts',
     out: './drizzle',
     dialect: 'postgresql',
     dbCredentials: {
-        url: 'postgres://aion_user:aion_password@localhost:5432/aion_db',
+        url: process.env.DATABASE_URL,
     },
 });
 //# sourceMappingURL=drizzle.config.js.map
